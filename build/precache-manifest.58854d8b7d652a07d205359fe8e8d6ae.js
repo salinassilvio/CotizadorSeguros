e9cd2812df797c2809458bbdeff88aa3f576f261
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2020-07-11 23:28:33 -0600

    First Commit

---
+++ build/precache-manifest.58854d8b7d652a07d205359fe8e8d6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635471b24b73a3390c1bb1efb770c0e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e1440c35d63b773df5",
+    "url": "/static/css/main.83ad9bad.chunk.css"
+  },
+  {
+    "revision": "9446c8e27822c0f96afd",
+    "url": "/static/js/2.9abcad79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9abcad79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e1440c35d63b773df5",
+    "url": "/static/js/main.e9679e6e.chunk.js"
+  },
+  {
+    "revision": "67e45de583dadb924782",
+    "url": "/static/js/runtime-main.620d9de5.js"
+  }
+]);
